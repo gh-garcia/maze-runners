8e48576ac9e1640b0ce98c659469b634805e904e
masquedphoque <gh985653@gmail.com>
2024-10-30 20:53:21 -0400

    alpha

---
+++ alpha/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace alpha
+{
+    class Program
+    {
+        static void Main()
+        {
+            int WIDTH = 14;
+            int HEIGHT = 14;
+
+            string[,] grid = new string[HEIGHT,WIDTH];
+
+            for (int i = 0; i < HEIGHT; i++ )
+            {
+                for (int j = 0; j < WIDTH; j++)
+                {
+                    grid[i,j] = " ";
+                }
+            }
+
+            for (int i = 0; i < HEIGHT; i++ )
+            {
+                for (int j = 0; j < WIDTH; j++)
+                {
+                    Console.Write(grid[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            
+            
+        }
+    }
+}
