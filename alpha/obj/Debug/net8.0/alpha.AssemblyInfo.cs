e84b5b4fc2656be7f6d5d4752ffb77c3e9163200
gh-garcia <gabyhg000@proton.me>
2025-01-30 20:34:53 -0500

    cambios raros aqui

--- alpha/obj/Debug/net8.0/alpha.AssemblyInfo.cs
+++ alpha/obj/Debug/net8.0/alpha.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("alpha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fdeaf21791badd1b173c29803dec63d4df70e9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b9f4f8542030a71e1051066cc5e702c9308ac47")]
 [assembly: System.Reflection.AssemblyProductAttribute("alpha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("alpha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
